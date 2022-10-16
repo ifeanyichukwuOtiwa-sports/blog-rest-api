5b037ea2154357c25a086ea23182f9f6f3c2ebc7
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-16 17:27:28 +0100

    fix failing test

--- src/test/java/io/regent/blogrestapi/BlogRestApiApplicationTests.java
+++ src/test/java/io/regent/blogrestapi/BlogRestApiApplicationTests.java
@@ -1,13 +1,31 @@
 package io.regent.blogrestapi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import io.regent.blogrestapi.config.TestConfig;
 
 @SpringBootTest
+@ContextConfiguration(
+        classes = {
+                BlogRestApiApplication.class
+        },
+        initializers = {
+                TestConfig.Initializer.class
+        }
+)
 class BlogRestApiApplicationTests {
+
+    @Autowired
+    protected BlogRestApiApplication application;
 
     @Test
     void contextLoads() {
+        assertThat(application).isNotNull();
     }
 
 }
