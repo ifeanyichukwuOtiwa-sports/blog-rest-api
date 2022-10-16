689955c013803cc53f91f31a74443f9cad77b5a1
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-16 15:15:49 +0100

    Initial commit for Blog Rest Api Project

---
+++ src/test/java/io/regent/blog/blogrestapi/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.regent.blog.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
